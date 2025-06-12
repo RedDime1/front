566681cee06763c45e980971015b6d8908a305e0
RedDime1 <dima.dima0029@gmail.com>
2025-06-12 17:15:56 +0300

    add return pages

--- logout.js
+++ logout.js
@@ -2,3 +2,11 @@ function logout() {
     localStorage.removeItem('jwt');
     window.location.reload();
 }
+
+function returnPage() {
+    const returnUrl = document.referrer && document.referrer.includes(window.location.host)
+        ? document.referrer
+        : 'index.html';
+
+    window.location.href = returnUrl;
+}
